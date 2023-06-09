5d0ae89567eaeacae8f96e5235e1fe9241e7796a
codewithmk21 <136069350+codewithmk21@users.noreply.github.com>
2023-06-09 15:10:08 +0530

    Update FindTheCelebrity.java

--- FindTheCelebrity.java
+++ FindTheCelebrity.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Created by gouthamvidyapradhan on 25/11/2017.
+ * Created by  codewithmk21 on 25/11/2017.
  *
  * <p>Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may
  * exist one celebrity. The definition of a celebrity is that all the other n - 1 people know
